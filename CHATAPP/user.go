2445f23a57deeb287676e4aa7ce13b4c579855dc
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-30 23:44:51 +0100

    Write Message Method Added to User

--- CHATAPP/user.go
+++ CHATAPP/user.go
@@ -1,6 +1,9 @@
 package CHATAPP
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type User struct {
 	username	string
@@ -9,5 +12,14 @@ type User struct {
 }
 
 func (user *User) readInput(s *Server) {
+	user.writeMessage(fmt.Sprintf("Welcome to Adebayo Chat App, " +
+		"Please Update your username and continue with other operations...\n"))
+	//for {
+	//	input, err := bufio.NewReader(user.conn).ReadString('\n')
+	//	checkError(err, fmt.Sprintf("Error Reading strings: %v", err))
+	//}
+}
 
+func (user *User) writeMessage(msg string) {
+	user.conn.Write([]byte(msg))
 }
