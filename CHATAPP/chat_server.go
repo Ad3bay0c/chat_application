be63c431e9312e942559916c03f39252613db66f
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-30 23:34:16 +0100

    Start Server and Handle Request Functions Added

--- CHATAPP/chat_server.go
+++ CHATAPP/chat_server.go
@@ -1,6 +1,50 @@
 package CHATAPP
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 type Server struct {
 	chats			map[string]*Chat
 	instructions	chan Instruction
+}
+
+func StartServer() {
+	s := &Server{
+		chats: make(map[string]*Chat),
+		instructions: make(chan Instruction),
+	}
+
+	listener, err := net.Listen("tcp", "3333")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		log.Printf("Server Started at localhost:3333")
+
+		conn, err := listener.Accept()
+		checkError(err, fmt.Sprintf("Error Accepting Request: %v", err))
+
+		go s.handleRequest(conn)
+	}
+}
+
+func (s *Server) handleRequest(conn net.Conn) {
+	log.Printf("A new Connection to the Server : %s", conn.RemoteAddr().String())
+
+	newUser := &User{
+		conn: conn,
+		username: "anonymous",
+	}
+
+	newUser.readInput(s)
+
+}
+func checkError(err error, msg string) {
+	if err != nil {
+		log.Printf(msg)
+	}
 }
