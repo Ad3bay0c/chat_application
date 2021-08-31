44892c82cc0fe58c3b93a20d15fa64d25866a328
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-31 01:08:45 +0100

    BroadCast Messaga method Added

--- CHATAPP/chat.go
+++ CHATAPP/chat.go
@@ -6,3 +6,11 @@ type Chat struct {
 	name	string
 	members	map[net.Addr]*User
 }
+
+func (chat *Chat) broadcast(user *User, msg string) {
+	for userAddr, u := range chat.members {
+		if userAddr != user.conn.RemoteAddr() {
+			u.writeMessage(user, msg)
+		}
+	}
+}
