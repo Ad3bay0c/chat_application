52170aad7b5620c120264e6e2d53bcfae9d89746
Ad3bay0c <aclintonadebayo@gmail.com>
2021-09-02 23:13:24 +0100

    Testing for Adding Client

--- CHATAPP/chat_test.go
+++ CHATAPP/chat_test.go
@@ -1,1 +1,37 @@
 package CHATAPP
+
+import (
+	"net"
+	"testing"
+)
+
+func TestServer_StartServer(t *testing.T) {
+	StartServer()
+}
+
+func TestStartServer(t *testing.T) {
+	tables := []struct{
+		output	int
+		word	string
+	}{
+		{
+			output: 1,
+			word: "First Client",
+		},
+		{
+			output: 2,
+			word: "Second Client",
+		},
+	}
+
+
+	for _, table := range tables {
+		t.Run(table.word, func(t *testing.T) {
+			conn, err := net.Dial("tcp", "localhost:3333")
+			if err != nil {
+				t.Errorf("Error")
+				conn.Close()
+			}
+		})
+	}
+}
